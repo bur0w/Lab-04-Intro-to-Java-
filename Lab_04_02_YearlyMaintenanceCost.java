de6d2b4658a71fc08a864d3b23fc791a49675741
bur0w <126435446+bur0w@users.noreply.github.com>
2024-02-05 21:07:42 -0500

    Create Lab_04_02_YearlyMaintenanceCost.java

---
+++ Lab_04_02_YearlyMaintenanceCost.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        double springCost = 20.00;
+        double summerCost = 10.00;
+        double fallCost = 18.00;
+        double winterCost = 25.00;
+        double totalYearlyCost = springCost + summerCost + fallCost + winterCost;
+
+        System.out.println("Spring Maintenance Cost: $" + springCost);
+        System.out.println("Summer Maintenance Cost: $" + summerCost);
+        System.out.println("Fall Maintenance Cost: $" + fallCost);
+        System.out.println("Winter Maintenance Cost: $" + winterCost);
+
+        System.out.println("\nTotal Yearly Maintenance Cost: $" + totalYearlyCost);
+    }
+}
